c6f626fbd0230c69f47ee22a4dab7312416ba308
ranjit739 <98301777+ranjit739@users.noreply.github.com>
2024-12-25 09:08:14 +0530

    Add files via upload

---
+++ src/route/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import Cookies from 'js-cookie'; // Import js-cookie to access cookies
+import Header from '../components/Header';
+
+const ProtectedRoute = ({ element }) => {
+  const token = Cookies.get('auth_token'); // Get the token from cookies
+
+
+console.log("token11",token)
+
+  // If no token is found, redirect to the login page
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // If the token exists, render the element (the component)
+  return(
+  <><Header />
+  {element}
+  </> 
+  )
+};
+
+export default ProtectedRoute;
