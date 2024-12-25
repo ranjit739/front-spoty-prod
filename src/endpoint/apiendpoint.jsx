a78553f9e65b796ecff2ebe26925165bc0c05f80
ranjit739 <98301777+ranjit739@users.noreply.github.com>
2024-12-25 11:59:20 +0530

    Update apiendpoint.jsx

--- src/endpoint/apiendpoint.jsx
+++ src/endpoint/apiendpoint.jsx
@@ -1,15 +1,11 @@
-// src/config/apiEndpoints.js
 
-export const API_BASE_URL = "https://example.com/api"; // Replace with your backend URL
+
 
 export const API_ENDPOINTS = {
   LOGIN: "/login",
   SIGNUP: `/register`,
-  // CREATEPLAYLIST:"/playlist",
-  // UPDATEPLAYLIST: "/playlist",
-  // GETPLAYLIST:"/playlist",
-  // DELETE:"/playlist",
-  // RENAME:"/playlist",
+ 
   PLAYLISTS:"/playlist",
+  TRACKS:"/track"
 
 };
